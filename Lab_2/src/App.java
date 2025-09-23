98aa9b2bb969e450d366638f906b15737ea7e8ad
Vianpyro <vianney@veremme.org>
2025-09-23 14:55:28 +0000

    Refactor App class; reorganize drawing logic and update user prompts for better clarity

--- Lab_2/src/App.java
+++ Lab_2/src/App.java
@@ -9,56 +9,57 @@ public class App {
     public static void main(String[] args) {
         List<ComposantDessin> dessins = new ArrayList<>();
 
-        // Dessin avec trois niveaux
-        Dessin dessin1 = new Dessin("Dessin1");
-        Cercle cercle1 = new Cercle("Cercle1");
-        Dessin dessin2 = new Dessin("Dessin2");
-        Dessin dessin3 = new Dessin("Dessin3");
-        Cercle cercle2 = new Cercle("Cercle2");
-        Cercle cercle3 = new Cercle("Cercle3");
-        Dessin dessin4 = new Dessin("Dessin4");
-        Triangle triangle1 = new Triangle("Triangle1");
-        Triangle triangle2 = new Triangle("Triangle2");
-        Rectangle rectangle1 = new Rectangle("Rectangle1");
-
-        dessin3.ajouter(cercle2);
-        dessin3.ajouter(cercle3);
-        dessin4.ajouter(triangle1);
-        dessin4.ajouter(triangle2);
-        dessin2.ajouter(dessin3);
-        dessin2.ajouter(dessin4);
-        dessin2.ajouter(rectangle1);
-        dessin1.ajouter(cercle1);
-        dessin1.ajouter(dessin2);
+        // "Dessin" avec une seule forme
+        Forme formeSeule = new Forme("etoile");
+        dessins.add(formeSeule);
 
         // Dessin avec deux formes seulement
         Dessin dessinSimple = new Dessin("DessinSimple");
         Carre carre = new Carre("Carre");
         Forme losange = new Forme("Losange");
+
+        dessins.add(dessinSimple);
         dessinSimple.ajouter(carre);
         dessinSimple.ajouter(losange);
 
-        // Dessin avec une seule forme
-        Forme formeSeule = new Forme("etoile");
+        // Dessin avec trois niveaux
+        Dessin dessinComplexe = new Dessin("Dessin1");
+        Dessin dessin2 = new Dessin("Dessin2");
+        Dessin dessin3 = new Dessin("Dessin3");
+        Dessin dessin4 = new Dessin("Dessin4");
+        Cercle cercle1 = new Cercle("Cercle1");
+        Cercle cercle2 = new Cercle("Cercle2");
+        Cercle cercle3 = new Cercle("Cercle3");
+        Rectangle rectangle1 = new Rectangle("Rectangle1");
+        Triangle triangle1 = new Triangle("Triangle1");
+        Triangle triangle2 = new Triangle("Triangle2");
 
-        dessins.add(dessin1);
-        dessins.add(dessinSimple);
-        dessins.add(formeSeule);
+        dessins.add(dessinComplexe);
+        dessinComplexe.ajouter(cercle1);
+        dessinComplexe.ajouter(dessin2);
+        dessin2.ajouter(dessin3);
+        dessin2.ajouter(dessin4);
+        dessin2.ajouter(rectangle1);
+        dessin3.ajouter(cercle2);
+        dessin3.ajouter(cercle3);
+        dessin4.ajouter(triangle1);
+        dessin4.ajouter(triangle2);
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println("Entrez le dessin à tester :");
-            System.out.println("1- Dessin avec trois niveaux");
+            System.out.println("1- Dessin avec un niveau (une forme seulement)");
             System.out.println("2- Dessin avec deux niveaux");
-            System.out.println("3- Dessin avec un niveau (une forme seulement)");
+            System.out.println("3- Dessin avec trois niveaux");
             System.out.print("Choisissez un dessin (1-3) : ");
             int choixDessin = scanner.nextInt();
 
             System.out.println("Entrez la strategie d'affichage à utiliser :");
             System.out.println("1- Indentation");
             System.out.println("2- Chemin complet");
-            System.out.print("Choisissez une strategie (1-2) : ");
+            System.out.println("3- Affichage graphique (non implémenté)");
+            System.out.print("Choisissez une strategie (1-3) : ");
             int choixStrategie = scanner.nextInt();
 
             int niveauMax = -1;
@@ -72,6 +73,13 @@ public class App {
                 strategie = new StrategieIndentation(niveauMax);
             } else if (choixStrategie == 2) {
                 strategie = new StrategieCheminComplet();
+            } else if (choixStrategie == 3) {
+                System.out.println(
+                        "Affichage graphique non implémenté, utilisation de la strategie par defaut (Indentation).");
+                strategie = new StrategieIndentation(-1);
+            } else {
+                System.out.println("Choix de strategie invalide.");
+                continue;
             }
 
             if (choixDessin >= 1 && choixDessin <= 3) {
